4980ea395bf848fef3073079f2b215b32d9fd48c
Niklaus <ad_mywork@163.com>
2021-10-15 09:08:25 +0800

    动态注册

--- app/src/main/cpp/jni_player.cpp
+++ app/src/main/cpp/jni_player.cpp
@@ -3,9 +3,30 @@
 
 #define LOG_TAG "jni_player"
 
-JNIEXPORT
-jint JNI_OnLoad(JavaVM* vm, void* reserved){
-    LOGI(LOG_TAG,"JNI_OnLoad");
+#define CLASS_FFMPEGPLAYER      "com/niklaus/player/FFmpegPlayer"
 
+static JNINativeMethod playerMethods[] = {
+        {"native_GetInfo",  "()Ljava/lang/String;", (void *) getInfo},
+};
+
+JNIEXPORT
+jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    LOGI(LOG_TAG, "JNI_OnLoad");
+    jint ret = JNI_ERR;
+    JNIEnv *env = nullptr;
+    if (vm->GetEnv((void **) (&env), JNI_VERSION_1_6) != JNI_OK) {
+        return ret;
+    }
+
+
+    jclass playerClazz = env->FindClass(CLASS_FFMPEGPLAYER);
+
+    env->RegisterNatives(playerClazz,playerMethods,sizeof(playerMethods) /
+                                                   sizeof(playerMethods[0]));
+}
+
+JNIEXPORT
+jstring getInfo(){
+    return "";
 }
 
